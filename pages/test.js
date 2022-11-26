f5e1ace6cc522aaa559f0ee33b3978d75177ca17
Marco Rapaccini <info@marcorapaccini.it>
2022-11-26 13:27:50 +0000

    Introduced metadata and related extraction for the post.

--- pages/test.js
+++ pages/test.js
@@ -1,11 +1,32 @@
 import {micromark} from 'micromark'
 import parse from 'html-react-parser';
-import postText from '../posts/test.md'
+import markdownPost from '../posts/test.md'
 
 const Test = () => {
+
+    const getPostObject = (markdownFile) => {
+        // extract the metadata part from the markdown file and remove new line chars, tabs and back quotes
+        const postArray = markdownFile.split("-----").slice(1);
+        return {
+            metadata: JSON.parse(postArray[0].replace(/[\n\t`]/g, '').trim()),
+            content: postArray[1]
+        }
+        // const textMetaData = RegExp("---(.*)---", "s").exec(markdownFile)[1].replace(/[\n\t`]/g, '').trim();
+    }
+
+    const postObject = getPostObject(markdownPost);
+    const metadata = postObject.metadata;
+    const content = postObject.content;
+
     return (
         <div>
-            {parse(micromark(postText))}
+            <div>
+                <p>Metadata</p>
+                <div>
+                    {Object.values(metadata).map( item => <p key={item.hash}>{item}</p>)}
+                </div>
+            </div>
+            {parse(micromark(content))}
         </div>
     );
 }
--- posts/test.md
+++ posts/test.md
@@ -1,3 +1,23 @@
+-----
+```
+{
+    "title": "This is an example post",
+    "author": "Marco Rapaccini",
+    "date": "2022-11-26",
+    "bannerImage": "url-to-image.png",
+    "tags": ["tag1", "tag2"]
+}
+```
+-----
 # Test
 ## About
 Hello World!
+```
+Multiple
+lines
+of code!
+```
+
+mmm
+
+`this is one line code`
