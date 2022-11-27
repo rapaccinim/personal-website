d603786aa575b6c18a9209cff679aee5947d835e
Marco Rapaccini <info@marcorapaccini.it>
2022-11-27 13:48:48 +0000

    Added layout for having header and footer in every page.

---
+++ components/layout.js
@@ -0,0 +1,13 @@
+import Header from "./header";
+import Footer from "./footer";
+
+const Layout = ({ children }) => {
+    return(
+        <div>
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    );
+}
+export default Layout;
