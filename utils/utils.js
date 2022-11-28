580a9ea092bde7fe378edf9786d2e29b0d18f4a9
Marco Rapaccini <info@marcorapaccini.it>
2022-11-28 19:51:55 +0000

    WIP: changed replaceAll method with more widely supported replace method.

--- utils/utils.js
+++ utils/utils.js
@@ -7,7 +7,7 @@ export const getRandomInt = (min, max) => {
 export const generateUUID = (optionalString = 'The Office (USA) is the best tv series ever') => {
     const lowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
     const uppercaseAlphabet = lowercaseAlphabet.toUpperCase();
-    const mixedString = lowercaseAlphabet + uppercaseAlphabet + optionalString.replaceAll(" ", "");
+    const mixedString = lowercaseAlphabet + uppercaseAlphabet + optionalString.replace(/\s/g, '');
     let randomString = '';
     for (let i = 0; i < mixedString.length; i++)
         randomString = randomString + mixedString.charAt(getRandomInt(0, mixedString.length - 1));
