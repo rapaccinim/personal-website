52f31a489b0debbf94ef5bbdf140a32737b1d50a
Marco Rapaccini <info@marcorapaccini.it>
2022-12-01 13:49:34 +0000

    Minimal change

--- pages/404.js
+++ pages/404.js
@@ -6,30 +6,32 @@ import styles from "../styles/404.module.css"
 const SeparationTildes = () => <p className={styles.separationTildes}>~ ~ ~</p>;
 
 const Custom404 = () => {
-    return <>
-        <h1>404 - Page Not Found</h1>
-        <div
-            className={styles.gif}
-        >
-            <Image
-                src={gif}
-                alt="404 page not found marco rapaccini web product development"
-                width="640px"
-                height="360px"
-            />
-        </div>
-        <SeparationTildes/>
-        <p>
-            Here is a rule to remember in future, when anything tempts you to feel bitter: not <i>This is misfortune</i>,
-            but <i>To bear this worthily is good fortune</i>.
-        </p>
-        <p>- Marcus Aurelius</p>
-        <SeparationTildes/>
-        <Link href="/">
-            <a>
-                Come back home, Lassie
-            </a>
-        </Link>
-    </>
+    return(
+        <>
+            <h1>404 - Page Not Found</h1>
+            <div
+                className={styles.gif}
+            >
+                <Image
+                    src={gif}
+                    alt="404 page not found marco rapaccini web product development"
+                    width="640px"
+                    height="360px"
+                />
+            </div>
+            <SeparationTildes/>
+            <p>
+                Here is a rule to remember in future, when anything tempts you to feel bitter: not <i>This is misfortune</i>,
+                but <i>To bear this worthily is good fortune</i>.
+            </p>
+            <p>- Marcus Aurelius</p>
+            <SeparationTildes/>
+            <Link href="/">
+                <a>
+                    Come back home, Lassie
+                </a>
+            </Link>
+        </>
+    )
 }
 export default Custom404;
